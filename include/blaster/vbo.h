2c2ed5fda1a2eb8a98f922b10cf6109b422d1b99
Lt-Henry <quiqueiii@gmail.com>
2017-09-18 17:00:01 +0200

    simplified vbo

--- include/blaster/vbo.h
+++ include/blaster/vbo.h
@@ -1,20 +1,20 @@
 /*
-	blaster
-	
-	Copyright (C) 2017  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+    blaster
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    Copyright (C) 2017  Enrique Medina Gremaldos <quiqueiii@gmail.com>
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef BLASTER_VBO
@@ -27,55 +27,42 @@ extern "C" {
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct {
-	/*! number of values */
-	uint8_t size;
-	
-	/*! data type */
-	uint8_t type;
-	
-	/*! settings mask */
-	uint8_t settings;
-	
-	/*! attribute name */
-	char* name;
-} bl_attribute_t;
 
 typedef struct {
-	/*! primitive type (triangles,lines,points) */
-	uint8_t primitive;
-	
-	/*! number of primitives */
-	size_t size;
-	
-	/*! struct storing attribute settings */
-	bl_attribute_t attribute[8];
-	
-	/*! pointer to vbo data */
-	void* data; 
-	
-	
+
+    /*! real number of vertices */
+    size_t size;
+
+    /*! maximum number of vertices */
+    size_t capacity;
+    
+    /*! number of attributes per vertex */
+    size_t attributes;
+    
+    /*! pointer to vbo data */
+    float* data; 
+
 } bl_vbo_t;
 
 /*!
-	Create a new vbo of given size
+    Create a new vbo of given size
 */
-bl_vbo_t* bl_vbo_new(uint8_t primitive,size_t size,void* data);
+bl_vbo_t* bl_vbo_new(size_t size,size_t attributes);
 
 /*!
-	Destroys vbo
+    Destroys vbo
 */
 void bl_vbo_delete(bl_vbo_t* vbo);
 
+/*!
+    Clear vertex count
+*/
+void bl_vbo_clear(bl_vbo_t* vbo);
 
 /*!
-	Sets up and attribute
-	\param id an integer from 0 to 7
-	\param type the type of the attribute, usually BL_FLOAT
-	\param size number of values, ie: 2 for a UV coords
-	\param name just a representative name
+    Adds a vertex
 */
-int bl_vbo_set_attribute(bl_vbo_t* vbo,int id,uint8_t type,uint8_t size,uint8_t settings,const char* name);
+void bl_vbo_add(bl_vbo_t* vbo,...);
 
 #ifdef __cplusplus
 }
--- src/vbo.c
+++ src/vbo.c
@@ -1,64 +1,67 @@
 /*
-	blaster
-	
-	Copyright (C) 2017  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+    blaster
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    Copyright (C) 2017  Enrique Medina Gremaldos <quiqueiii@gmail.com>
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
 #include <blaster/vbo.h>
 #include <blaster/constants.h>
 
+#include <stdarg.h>
 
-bl_vbo_t* bl_vbo_new(uint8_t primitive,size_t size,void* data)
+
+bl_vbo_t* bl_vbo_new(size_t size,size_t attributes)
 {
-	bl_vbo_t* vbo=NULL;
-	
-	vbo=malloc(sizeof(bl_vbo_t));
-	
-	memset(vbo,0,sizeof(bl_vbo_t));
-	
-	vbo->primitive=primitive;
-	vbo->size=size;
-	vbo->data=data;
-	
-	return vbo;
+    bl_vbo_t* vbo=NULL;
+
+    vbo=malloc(sizeof(bl_vbo_t));
+
+    vbo->capacity=size;
+    vbo->size=0;
+    vbo->attributes=attributes;
+    vbo->data=malloc(sizeof(float)*size*attributes);
+
+    return vbo;
 }
 
 void bl_vbo_delete(bl_vbo_t* vbo)
 {
-
-	for (int n=0;n<8;n++) {
-		if (vbo->attribute[n].name!=NULL) {
-			free(vbo->attribute[n].name);
-		}
-	}
-	
-	free(vbo);
+    free(vbo->data);
+    free(vbo);
 }
 
-int bl_vbo_set_attribute(bl_vbo_t* vbo,int id,uint8_t type,uint8_t size,uint8_t settings,const char* name)
+void bl_vbo_clear(bl_vbo_t* vbo)
 {
-	if (id<0 || id>7) {
-		return 1;
-	}
-	
-	vbo->attribute[id].type=type;
-	vbo->attribute[id].size=size;
-	vbo->attribute[id].settings=settings;
-	vbo->attribute[id].name=strdup(name);
-	
-	return 0;
+    vbo->size=0;
+}
+
+void bl_vbo_add(bl_vbo_t* vbo,...)
+{
+    float* ptr = &vbo->data[vbo->size*vbo->attributes];
+    
+    va_list args;
+    
+    va_start(args,vbo);
+    
+    for (int n=0;n<vbo->attributes;n++) {
+        ptr[n]=va_arg(args,float);
+    }
+    
+    va_end(args);
+    
+    vbo->size++;
 }
