a865b5ef46e9eccbc6fc81cdfceb3af439a9b909
Lt-Henry <quiqueiii@gmail.com>
2018-04-18 16:57:16 +0200

    refactoring matrix

--- include/blaster/matrix.h
+++ include/blaster/matrix.h
@@ -24,61 +24,64 @@
 extern "C" {
 #endif
 
+typedef struct {
+    float data[16];
+} bl_matrix_t;
 
 /*!
     Sets a value in row/column
 */
-void bl_matrix_set(float* m,int r,int c,float v);
+void bl_matrix_set(bl_matrix_t* m,int r,int c,float v);
 
 /*!
     Gets a value from row/column
 */
-float bl_matrix_get(float* m,int r,int c);
+float bl_matrix_get(bl_matrix_t* m,int r,int c);
 
 /*!
     Copy matrices
 */
-void bl_matrix_copy(float* d,float* s);
+void bl_matrix_copy(bl_matrix_t* d,bl_matrix_t* s);
 
 /*!
     Sets identity matrix
 */
-void bl_matrix_identity(float* m);
+void bl_matrix_identity(bl_matrix_t* m);
 
 /*!
     Sets translation matrix
 */
-void bl_matrix_translate(float* m,float x,float y,float z);
+void bl_matrix_translate(bl_matrix_t* m,float x,float y,float z);
 
 /*!
     Sets rotation matrix over X
 */
-void bl_matrix_rotate_x(float* m,float rads);
+void bl_matrix_rotate_x(bl_matrix_t* m,float rads);
 
 /*!
     Sets rotation matrix over Y
 */
-void bl_matrix_rotate_y(float* m,float rads);
+void bl_matrix_rotate_y(bl_matrix_t* m,float rads);
 
 /*!
     Sets rotation matrix over Z
 */
-void bl_matrix_rotate_z(float* m,float rads);
+void bl_matrix_rotate_z(bl_matrix_t* m,float rads);
 
 /*!
     Sets a scale matrix
 */
-void bl_matrix_scale(float* m,float x,float y,float z);
+void bl_matrix_scale(bl_matrix_t* m,float x,float y,float z);
 
 /*!
     Matrix multiplication
 */
-void bl_matrix_mult(float* m,float* a,float* b);
+void bl_matrix_mult(bl_matrix_t* m,bl_matrix_t* a,bl_matrix_t* b);
 
 /*!
     Print matrix to stdout
 */
-void bl_matrix_print(const float* m);
+void bl_matrix_print(const bl_matrix_t* m);
 
 #ifdef __cplusplus
 }
